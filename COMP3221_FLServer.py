8dc2711469691003e6508c2ef3cd09eb425b1df2
= <gen.lee025@gmail.com>
2022-05-20 11:39:45 +1000

    server

--- COMP3221_FLServer.py
+++ COMP3221_FLServer.py
@@ -1,5 +1,5 @@
 import sys
-
+import threading 
 
 class Server:
     def __init__(self):
@@ -7,8 +7,19 @@ class Server:
         self.subsample_clients = sys.argv[2]  # 0 for use all the clients' weights, 1 use only 2 chosen randomly
 
     def run(self):
-        pass
+        start_wss_thread = threading.Thread(target=self.start_websocket_server)
+        start_wss_thread.start()
+    
 
+    def start_websocket_server(self):
+        """
+        This is the method that starts the websocket server. The handling clients thread is called when a client connects to the websocket server. 
+        """
+        self.server.listen()
+        while True:
+            conn, addr = self.server.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, ))
+            thread.start()
 
 server = Server()
 server.run()
