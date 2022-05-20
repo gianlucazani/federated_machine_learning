82d7dd35d98cacf590932350de968fc03b079983
= <gen.lee025@gmail.com>
2022-05-20 13:11:55 +1000

    client

--- COMP3221_FLClient.py
+++ COMP3221_FLClient.py
@@ -2,7 +2,9 @@ import sys
 import os
 import numpy as np
 import json
-
+import socket
+import threading
+import _pickle
 
 HOST = "127.0.0.1"
 
@@ -10,17 +12,56 @@ HOST = "127.0.0.1"
 class Client:
     def __init__(self):
         self.id = sys.argv[1]
-        self.port_no = sys.argv[2]
+        self.port_no = int(sys.argv[2])
         self.server_port_no = 6000  # fixed to 6000
         self.optimization_method = sys.argv[3]  # 0 for GD, 1 for Mini-Batch GD
         self.X_train = []
         self.Y_train = []
         self.X_test = []
         self.Y_test = []
-
+        self.log_file = os.path.join("./","logs", "client"+str(self.id)+"_log.txt")
+        print(self.log_file)
+        self.read_dataset()
+        self.server =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((HOST, int(self.port_no)))
+        handshake_thread = threading.Thread(target=self.handshake())
+        handshake_thread.start()
 
     def run(self):
-        self.read_dataset()
+        self.server.listen()
+        while True:
+            with open(self.log_file, 'w') as f:
+                f.write(f'I am client {self.id}')
+                conn, addr = self.server.accept()
+                f.write(f'Receving new global model')
+                msg = _pickle.loads(conn.recv(4096))
+                W = msg['W']
+                training_loss = self.softmax_loss(self.X_train, self.Y_train, W)
+                f.write(f'Training loss: {training_loss}')
+                y_pre = self.pred(W, self.X_test)
+                testing_accuracy = self.accuracy(y_pre, self.Y_test)
+                f.write(f'Testing acurancy: {testing_accuracy}')
+                f.write(f'Local training...')
+                W, loss_hist = self.softmax_fit(self.X_train, self.Y_train, W)
+                msg = {
+                    'W': W,
+                    'id': self.id
+                }
+                conn.sendall(_pickle.dumps(msg))
+                if msg['rounds_left'] ==0:
+                    break
+
+    def handshake(self):
+        try: 
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((HOST, self.server_port_no))
+                packet = {
+                    'id': int(self.id),
+                    'port': int(self.port_no)
+                }
+                s.sendall(_pickle.dumps(packet))
+        except:
+            pass
 
     def read_dataset(self):
         train_path = os.path.join("FLdata", "train", "mnist_train_client" + str(self.id) + ".json")
@@ -34,6 +75,8 @@ class Client:
             test = json.load(f_test)
             test_data.update(test['user_data']) 
         self.X_train, self.Y_train, self.X_test, self.Y_test = train_data['0']['x'], train_data['0']['y'], test_data['0']['x'], test_data['0']['y']
+        self.X_train = np.concatenate((self.X_train, np.ones((self.X_train.shape[0], 1))), axis = 1)
+        self.X_test = np.concatenate((self.X_test, np.ones((self.X_test.shape[0], 1))), axis = 1)
 
     def softmax(self, X, W):
         Z = X.dot(W)
