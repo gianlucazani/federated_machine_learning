b1b0cc01b39d37edefc3a81bafa328fcd83186cd
Gianluca Zani <zani.gianluca98@gmail.com>
2022-05-21 14:56:32 +1000

    Server works with 5 clients and subsampling as well

--- COMP3221_FLClient.py
+++ COMP3221_FLClient.py
@@ -6,64 +6,126 @@ import socket
 import threading
 import _pickle
 
-CLIENT_HOST = "127.0.0.1"
-SERVER_HOST = "127.0.0.1"
+HOST = "127.0.0.1"
+
+
+def receive_all(sock):
+    """
+    Due to high dimensional packets (about 60k bytes) exchanged between server and client, we need to make the read of the received message incremental,
+    with steps equal to the maximum supported size: 4096 bytes.
+    :param sock: socket to receive from
+    :return: the whole data in bytes
+    """
+    BUFF_SIZE = 4096  # 4 KiB
+    data = b''
+    while True:
+        part = sock.recv(BUFF_SIZE)
+        data += part
+        if len(part) < BUFF_SIZE:
+            # either 0 or end of data
+            break
+    return data
+
 
 class Client:
     def __init__(self):
         self.id = sys.argv[1]
         self.port_no = int(sys.argv[2])
-        self.server_port_no = 6000  # fixed to 6000
         self.optimization_method = sys.argv[3]  # 0 for GD, 1 for Mini-Batch GD
-        self.X_train = []
-        self.Y_train = []
-        self.X_test = []
-        self.Y_test = []
-        self.log_file = os.path.join("./","client"+str(self.id)+"_log.txt")
-        print(self.log_file)
+
+        self.server_port_no = 6000  # fixed to 6000
+
+        # LOG FILE
+        self.log_file = os.path.join("./", "client" + str(self.id) + "_log.txt")
+        # clean file before starting
+        with open(self.log_file, 'w') as f:
+            f.write("")
+
+        # RETRIEVE DATASET
+        self.X_train = np.array([])
+        self.Y_train = np.array([])
+        self.X_test = np.array([])
+        self.Y_test = np.array([])
         self.read_dataset()
-        self.server =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((CLIENT_HOST, int(self.port_no)))
+
+        # SOCKET
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.bind((HOST, int(self.port_no)))
         handshake_thread = threading.Thread(target=self.handshake())
         handshake_thread.start()
 
     def run(self):
-        self.server.listen()
-        while True:
-            with open(self.log_file, 'w+') as f:
-                f.write(f'I am client {self.id}')
-                conn, addr = self.server.accept()
-                f.write(f'Receving new global model')
-                msg = _pickle.loads(conn.recv(65536))
-                W = msg['W']
-                training_loss = self.softmax_loss(self.X_train, self.Y_train, W)
-                f.write(f'Training loss: {training_loss}')
-                y_pre = self.pred(W, self.X_test)
-                testing_accuracy = self.accuracy(y_pre, self.Y_test)
-                f.write(f'Testing acurancy: {testing_accuracy}')
-                f.write(f'Local training...')
-                W, loss_hist = self.softmax_fit(self.X_train, self.Y_train, W)
-                msg = {
-                    'W': W,
-                    'id': self.id
-                }
-                conn.sendall(_pickle.dumps(msg))
-                if msg['rounds_left'] ==0:
-                    break
+        rounds_left = 100
+        while rounds_left > 0:
+            try:
+                self.client_socket.listen()
+                with open(self.log_file, 'a+') as f:
+
+                    # RECEIVE GLOBAL MODEL
+                    print(f'I am client {self.id}')
+                    conn, addr = self.client_socket.accept()
+                    print(f'Receiving new global model')
+                    received = receive_all(conn)
+                    received_global_model = _pickle.loads(received)
+                    # print(f"-- Received a packet with dimension: {sys.getsizeof(received)} --")
+                    W = received_global_model['W']
+
+                    # CALCULATE TRAINING LOSS
+                    training_loss = self.softmax_loss(self.X_train, self.Y_train, W)
+                    print(f'Training loss: {training_loss}')
+
+                    # PREDICT USING THE GLOBAL MODEL AND TEST ACCURACY
+                    y_predictions = self.predict(W, self.X_test)
+                    testing_accuracy = self.accuracy(y_predictions, self.Y_test)
+                    print(f'Testing accuracy: {testing_accuracy}')
+
+                    # TRAIN THE MODEL WITH GD or MINI-BATCH GD
+                    print(f'Local training...')
+                    W, loss_hist = self.softmax_fit(self.X_train, self.Y_train, W)
+                    updated_weights = {
+                        'W': W,
+                        'id': str(self.id)
+                    }
+
+                    # SEND UPDATED MODEL BACK TO SERVER
+                    # print(f"-- Sending a packet with dimension: {sys.getsizeof(_pickle.dumps(updated_weights))} -- ")
+                    conn.sendall(_pickle.dumps(updated_weights))
+                    rounds_left = received_global_model['rounds_left']
+
+                    # WRITE TO FILE
+                    # we might choose to write something different to the file, maybe something easier to be read when performing final evaluation
+                    # we could print data in a fancier way (maybe .csv)
+                    # the text about what the client is doing is only left to be printed at terminal but not in the log file
+
+                    # Writing is moved down here for catching exception separately
+                    try:
+                        f.write(f'I am client {self.id} \n')
+                        f.write(f'Receiving new global model \n')
+                        f.write(f'Training loss: {training_loss} \n')
+                        f.write(f'Testing accuracy: {testing_accuracy} \n')
+                        f.write(f'Local training... \n')
+                    except IOError as e:
+                        print(f"Client {self.id} error writing to log file")
+                        print(f"ERROR: {e}")
+            except socket.error as e:
+                print(f"Client {self.id} error while communicating with server")
+                print(f"ERROR: {e}")
+                continue
 
     def handshake(self):
-        try: 
-            print("trying handshake")
+        try:
+            print("Sending handshake")
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect((SERVER_HOST, self.server_port_no))
+                s.connect((HOST, self.server_port_no))
                 packet = {
-                    'id': int(self.id),
-                    'port': int(self.port_no),
-                    'datasize': self.X_train.shape[1]
+                    'id': str(self.id),
+                    'port_no': int(self.port_no),
+                    'data_size': self.X_train.shape[0]
                 }
                 s.sendall(_pickle.dumps(packet))
-        except:
-            print("couldnt connect to host")
+        except socket.error as e:
+            print(f"Client {self.id} cannot send handshake")
+            print(f"ERROR: {e}")
             pass
 
     def read_dataset(self):
@@ -76,60 +138,57 @@ class Client:
             train_data.update(train['user_data'])
         with open(os.path.join(test_path), "r") as f_test:
             test = json.load(f_test)
-            test_data.update(test['user_data']) 
-        self.X_train, self.Y_train, self.X_test, self.Y_test = train_data['0']['x'], train_data['0']['y'], test_data['0']['x'], test_data['0']['y']
+            test_data.update(test['user_data'])
+        self.X_train, self.Y_train, self.X_test, self.Y_test = train_data['0']['x'], train_data['0']['y'], \
+                                                               test_data['0']['x'], test_data['0']['y']
         self.X_train = np.array(self.X_train)
         self.Y_train = np.array(self.Y_train)
+        self.Y_train = self.Y_train.astype(int)
         self.X_test = np.array(self.X_test)
         self.Y_test = np.array(self.Y_test)
-        self.X_train = np.concatenate((self.X_train, np.ones((self.X_train.shape[0], 1))), axis = 1)
-        self.X_test = np.concatenate((self.X_test, np.ones((self.X_test.shape[0], 1))), axis = 1)
+        self.X_train = np.concatenate((self.X_train, np.ones((self.X_train.shape[0], 1))), axis=1)
+        self.X_test = np.concatenate((self.X_test, np.ones((self.X_test.shape[0], 1))), axis=1)
 
     def softmax(self, X, W):
         Z = X.dot(W)
         e_Z = np.exp(Z)
-        A = e_Z / e_Z.sum(axis = 1, keepdims = True)
+        A = e_Z / e_Z.sum(axis=1, keepdims=True)
         return A
-    
+
     def softmax_loss(self, X, y, W):
-        A = self.softmax(X,W)
-        print(A.shape)
-        id0 = range(X.shape[0])
-        print(id0)
+        A = self.softmax(X, W)
+        id0 = np.arange(X.shape[0])
         return -np.mean(np.log(A[id0, y]))
 
     def softmax_grad(self, X, y, W):
-        pred = self.softmax(X,W)  
-        xid = range(X.shape[0])   
-        pred[xid, y] -= 1        
-        return X.T.dot(pred)/X.shape[0]
+        prediction = self.softmax(X, W)
+        xid = range(X.shape[0])
+        prediction[xid, y] -= 1
+        return X.T.dot(prediction) / X.shape[0]
 
-    def softmax_fit(self, X, y, W, lr = 0.01, nepoches = 2):
-        ep = 0 
-        loss_hist = [self.softmax_loss(X, y, W)] # store history of loss 
+    def softmax_fit(self, X, y, W, lr=0.7, epochs=2):
+        ep = 0
+        loss_hist = [self.softmax_loss(X, y, W)]  # store history of loss
         N = X.shape[0]
-        while ep < nepoches: 
-            ep += 1 
-            W -= lr*self.softmax_grad(X, y, W)
+        while ep < epochs:
+            ep += 1
+            W -= lr * self.softmax_grad(X, y, W)
             loss_hist.append(self.softmax_loss(X, y, W))
-        return W, loss_hist 
+        return W, loss_hist
 
-    def accuracy(self, y_pre, y):
-        diff = y_pre-y 
-        n_zeros = np.count_nonzero(diff==0)
-        print(f'correctly predicted: {n_zeros}')
+    def accuracy(self, y_predictions, y):
+        diff = y_predictions - y
+        n_zeros = np.count_nonzero(diff == 0)
+        # print(f'Correctly predicted: {n_zeros}')
         np.asanyarray(n_zeros)
-        accuracy = n_zeros/len(y)
-        print(accuracy)
+        accuracy = n_zeros / len(y)
+        # print(f"Accuracy: {accuracy}")
         return accuracy
 
-    def pred(self, W, X):
-        A = self.softmax(X,W)
-        return np.argmax(A, axis = 1)
+    def predict(self, W, X):
+        A = self.softmax(X, W)
+        return np.argmax(A, axis=1)
 
 
 client = Client()
 client.run()
-
-
-
--- COMP3221_FLServer.py
+++ COMP3221_FLServer.py
@@ -1,4 +1,5 @@
 import _pickle
+import random
 import socket
 import sys
 import threading
@@ -6,8 +7,25 @@ import time
 
 import numpy as np
 
-SERVER_HOST = "127.0.0.1"
-CLIENT_HOST = "127.0.0.1"
+HOST = "127.0.0.1"
+
+
+def receive_all(sock):
+    """
+    Due to high dimensional packets (about 60k bytes) exchanged between server and client, we need to make the read of the received message incremental,
+    with steps equal to the maximum supported size: 4096 bytes.
+    :param sock: socket to receive from
+    :return: the whole data in bytes
+    """
+    BUFF_SIZE = 4096  # 4 KiB
+    data = b''
+    while True:
+        part = sock.recv(BUFF_SIZE)
+        data += part
+        if len(part) < BUFF_SIZE:
+            # either 0 or end of data
+            break
+    return data
 
 
 class HandshakeThread(threading.Thread):
@@ -21,67 +39,171 @@ class HandshakeThread(threading.Thread):
             conn, addr = self.server.server_socket.accept()
             received = conn.recv(32768)
             client = _pickle.loads(received)
-            self.server.clients.update(client)
+            print(f"Client connected: {client}")
+            self.server.clients.append(client)
+
 
 class Server:
     def __init__(self):
         self.port_no = sys.argv[1]  # fixed to 6000
-        self.subsample_clients = sys.argv[2]  # 0 for use all the clients' weights, 1 use only 2 chosen randomly
-        self.clients = dict()  # (key = ID, value = PORT)
-        self.clients_weights = dict()  # (key = ID, value = weight)
+        self.client_subsampling = sys.argv[2]  # 0 for use all the clients' models, 1 use only 2 chosen randomly
+        if self.client_subsampling == "0":
+            self.client_subsampling = False
+        else:
+            self.client_subsampling = True
         self.alive = False
+
+        self.clients = list()  # list of objects { "id": int, "port_no": int, "data_size": int }
+        self.clients_models = dict()  # (key = ID, value = model)
+
+        np.random.seed(123)
         self.W = np.random.randn(785, 10)
-        print(sys.getsizeof(self.W))
+
+        # print(sys.getsizeof(self.W))
+
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((SERVER_HOST, int(self.port_no)))
+        self.server_socket.bind((HOST, int(self.port_no)))
 
     def run(self):
         self.alive = True
         handshake_thread = HandshakeThread(self)
         self.server_socket.listen()
         conn, addr = self.server_socket.accept()
-        received = conn.recv(65536)
+        received = conn.recv(4096)
         client = _pickle.loads(received)
-        self.clients.update(client)
-        print("first client connected!!")
+        self.clients.append(client)
+        print(f"Client connected: {client}")
         handshake_thread.start()
-        time.sleep(3)
+        time.sleep(10)
         self.federated_learning()
+
+    def federated_learning(self):
+        for communication_round in range(50):
+            #with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            # BROADCAST GLOBAL MODEL AND RECEIVE UPDATED ONE FROM CLIENTS
+            self.send_and_receive_model(50 - 1 - communication_round)
+            # # BROADCAST GLOBAL MODEL TO ALL THE CLIENTS
+            # self.broadcast_W(5 - 1 - communication_round)
+            #
+            # # RECEIVE LOCAL MODELS FROM CLIENTS
+            # self.server_socket.listen()
+            # for i in range(len(self.clients)):
+            #     client_socket, address = self.server_socket.accept()
+            #     received = receive_all(client_socket)
+            #     received_clients_model = _pickle.loads(received)
+            #     client_id = received_clients_model["id"]
+            #     clients_model = received_clients_model["W"]
+            #     self.clients_models[client_id] = clients_model
+
+            # SELECT WHICH MODELS TO COMBINE FOR THE NEW GLOBAL MODEL
+            if self.client_subsampling:
+                selected_clients = self.subsample_clients()
+            else:
+                selected_clients = self.clients
+
+            # GET TOTAL DATA SIZE (sum of data sizes of the alive clients)
+            total_size = self.get_total_size()
+
+            # COMPUTE NEW GLOBAL MODEL
+            self.W = self.compute_new_global_model(selected_clients, total_size)
+
+            # CLEAN MODELS DICTIONARY FOR NEXT ITERATION
+            self.clients_models = dict()
+
+    def send_and_receive_model(self, rounds_left):
+        for client in self.clients:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as _socket:
+                try:
+                    # PREPARE MODEL PACKAGE
+                    package = {
+                        "W": self.W,
+                        "rounds_left": rounds_left
+                    }
+                    message = _pickle.dumps(package)
+                    # print(sys.getsizeof(message))
+
+                    # SEND MODEL
+                    _socket.connect((HOST, int(client["port_no"])))
+                    _socket.sendall(message)
+
+                    # RECEIVE UPDATED MODEL PACKAGE
+                    received = receive_all(_socket)
+                    received_clients_model = _pickle.loads(received)
+                    client_id = received_clients_model["id"]
+                    clients_model = received_clients_model["W"]
+                    self.clients_models[client_id] = clients_model
+
+                    print(f"Sent global model. Rounds left: {rounds_left}")
+                except socket.error as e:
+                    print(f"Server {self.port_no} error SENDING W to client {client['id']}")
+                    print(f"ERROR {e}")
+
+    # def broadcast_W(self, rounds_left: int):
+    #     """
+    #     Sends the current global model W to all the connected clients
+    #     :param _socket: socket used for sending and receiving models
+    #     :param rounds_left: number of global communication rounds left (used by clients for stopping themselves)
+    #     """
+    #     for client in self.clients:
+    #         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as _socket:
+    #             try:
+    #                 packet = {
+    #                     "W": self.W,
+    #                     "rounds_left": rounds_left
+    #                 }
+    #                 message = _pickle.dumps(packet)
+    #                 print(sys.getsizeof(message))
+    #                 _socket.connect((HOST, int(client["port_no"])))
+    #                 _socket.sendall(message)
+    #                 print(f"Sent global model. Rounds left: {rounds_left}")
+    #             except socket.error as e:
+    #                 print(f"Server {self.port_no} error SENDING W to client {client['id']}")
+    #                 print(f"ERROR {e}")
+
+    def compute_new_global_model(self, selected_clients, total_size):
+        result = np.zeros_like(self.get_model(selected_clients[0]))  # init the result to the same shape as the model but filled with 0, necessary for performing sum
+        for client in selected_clients:
+            model = self.get_model(client)
+            model = np.array(model)
+            coefficient = client["data_size"] / total_size
+            result += coefficient * model
+        return result
+
+    def subsample_clients(self):
+        """
+        Subsample clients if the subsampling option is set to True
+        :return: the list of the subsampled clients
+        """
+        match len(self.clients):
+            case 1:
+                return list([self.clients[0]])
+            case 2:
+                return list([self.clients[0], self.clients[1]])
+        randomly_selected = random.choices(self.clients, k=2)
+        return randomly_selected
+
+    def get_model(self, client):
+        """
+        Given a client, returns the model sent by the client
+        :param client: client dictionary { "id": int, "port_no": int, "data_size": int }
+        :return: client's model as np.array
+        """
+        client_id = client["id"]
+        model = np.array(self.clients_models[client_id])
+        return model
+
+    def get_total_size(self):
+        """
+        Get total data size of the currently alive clients
+        """
+        total_size = 0
+        for client in self.clients:
+            total_size += client["data_size"]
+        return total_size
 
     def is_alive(self) -> bool:
         return self.alive
 
-    def federated_learning(self):
-        for communication_round in range(5):
-            self.broadcast_W(100 - communication_round)
-            self.server_socket.listen()
-            conn, addr = self.server_socket.accept()
-            received = conn.recv(131072)
-            new_weight = _pickle.loads(received)
-            self.W = new_weight["W"]
-            # for client in range(len(self.clients.keys())):
-            #     self.server_socket.listen()
-            #     conn, addr = self.server_socket.accept()
-            #     received = conn.recv(4096)
-            #     new_weight = _pickle.loads(received)
-            #     # self.clients_weights[]
-            print(self.W)
-
-    def broadcast_W(self, rounds_left: int):
-        for client_id, destination_port in self.clients.items():
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                try:
-                    packet = {
-                        "W": self.W,
-                        "rounds_left": rounds_left
-                    }
-                    message = _pickle.dumps(packet)
-                    print(sys.getsizeof(message))
-                    s.connect((CLIENT_HOST, int(destination_port)))
-                    s.sendall(message)
-                except socket.error as e:
-                    print(f"Server {self.port_no} error SENDING W to client {client_id}")
-                    print(f"ERROR {e}")
 
 server = Server()
 server.run()
