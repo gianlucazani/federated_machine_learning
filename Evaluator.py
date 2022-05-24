2cc5ebe0ce4e096c78ae1bd0e3c1a5f5f47aab30
Gianluca Zani <zani.gianluca98@gmail.com>
2022-05-24 16:55:17 +1000

    Solved client joining later

--- Evaluator.py
+++ Evaluator.py
@@ -7,22 +7,17 @@ class Evaluator:
         self.files = files
 
     def run(self):
+
+        # Plot losses and accuracies of each client
         for file in self.files:
-            figure, axis = plt.subplots(2, 2)
             data = pandas.read_csv(file)
+            data.plot(x="communication_round", y="accuracy", color="blue")
+            data.plot(x="communication_round", y="loss", color="red")
 
-            axis[0, 0].plot(data["communication_round"], data["accuracy"], color="blue")
-            axis[0, 0].set_title("Client1")
-
-            # For Cosine Function
-            # axis[0, 1].plot(X, Y2)
-            # axis[0, 1].set_title("Cosine Function")
-            #
-            # data.plot(x="communication_round", y="accuracy", color="blue")
-            # data.plot(x="communication_round", y="loss", color="red")
         plt.show()
 
-files = ["evaluation_log_1.csv"]
+
+files = ["evaluation_log_1.csv", "evaluation_log_2.csv"]
 
 evaluator = Evaluator(files)
 evaluator.run()
